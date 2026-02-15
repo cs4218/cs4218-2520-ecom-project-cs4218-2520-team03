0cffb88b3f0391bbf137a9826d29d8f1e3a84308
Zihan <e0970553@u.nus.edu>
2026-02-15 11:36:22 +0800

    add unit tests for authMiddleware.js

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -6,12 +6,19 @@ export default {
   testEnvironment: "node",
 
   // which test to run
-  testMatch: ["<rootDir>/controllers/*.test.js"],
+  testMatch: [
+    "<rootDir>/controllers/*.test.js",
+    "<rootDir>/helpers/authHelper.test.js",
+    "<rootDir>/middlewares/authMiddleware.test.js"
+  ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["controllers/productController.js"],
-  coverageThreshold: {
+  collectCoverageFrom: [
+    "controllers/productController.js",
+    "helpers/authHelper.js",
+    "middlewares/authMiddleware.js"
+  ],  coverageThreshold: {
     global: {
       lines: 50,
       functions: 50,
