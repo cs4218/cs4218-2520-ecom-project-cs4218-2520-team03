ed2cc08fe7ce3414faecdcaa0da61cd7f6536e97
zhiruo <e0967827@u.nus.edu>
2026-03-21 20:50:37 +0800

    modify hard coded selection of products

--- tests/e2e/adminOrderManagement.spec.cjs
+++ tests/e2e/adminOrderManagement.spec.cjs
@@ -120,8 +120,6 @@ test('user should not be able to place an order with invalid card details', asyn
 
 test('admin should see order with failed payment status', async ({ page }) => {
   // 1) User places an order with invalid card details
-//   await page.getByRole('button', { name: 'More Details' }).nth(2).click();
-//   await page.getByRole('separator').first().click();
   const productCard = page.locator('.card').filter({
     has: page.getByText('Expensive Laptop', { exact: true }),
   });
@@ -200,7 +198,12 @@ test('admin should see quantity change after user places order', async ({ page }
   await page.getByRole('button', { name: 'LOGIN' }).click();
 
   // 3) User places an order of that product
-  await page.getByRole('button', { name: 'More Details' }).nth(3).click();
+  const productCard = page.locator('.card').filter({
+    has: page.getByText('Smart Watch Ultra', { exact: true }),
+  });
+
+  await expect(productCard).toBeVisible();
+  await productCard.getByRole('button', { name: 'More Details' }).click();
   await expect(await page.getByRole('heading', { name: 'Name : Smart Watch Ultra' })).toBeVisible();
   await page.getByRole('button', { name: 'ADD TO CART' }).first().click();
   await page.getByRole('button', { name: 'ADD TO CART' }).first().click();
